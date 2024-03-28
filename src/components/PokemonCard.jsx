3b56ffa8734ecc9dac04377e6b5a64b1a55a7ac9
Benjamin CHAILLAN <benjamin.chaillan-pro@laposte.net>
2024-03-28 17:23:17 +0100

    dynamic composant

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,22 @@
+const pokemonList = [
+  {
+    name: 'bulbasaur',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  },
+  {
+    name: 'mew',
+  },
+];
+
 const PokemonCard = () => {
+  const pokemon = pokemonList[0];
+
   return (
     <div>
       <figure>
-        <img
-          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
-          alt=''
-        />
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt='' /> : <p>???</p>}
+        <figcaption>{pokemon.name}</figcaption>
       </figure>
     </div>
   );
