3a81c7d2319b6ec3d4ee9b37862677541312e662
Benjamin CHAILLAN <benjamin.chaillan-pro@laposte.net>
2024-03-29 09:45:43 +0100

    props done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import PokemonCard from './components/PokemonCard';
 
+const pokemonList = [
+  {
+    name: 'bulbasaur',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+  },
+  {
+    name: 'mew',
+  },
+];
+const pokemon = pokemonList[0];
 function App() {
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,5 @@
-const pokemonList = [
-  {
-    name: 'bulbasaur',
-    imgSrc:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-  },
-  {
-    name: 'mew',
-  },
-];
-
-const PokemonCard = () => {
-  const pokemon = pokemonList[0];
-
+/* eslint-disable react/prop-types */
+const PokemonCard = ({ pokemon }) => {
   return (
     <div>
       <figure>
