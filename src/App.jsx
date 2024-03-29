22d9ef832216179af2342a72d5a3cd53cca3d79b
Benjamin CHAILLAN <benjamin.chaillan-pro@laposte.net>
2024-03-29 10:23:19 +0100

    quest dones

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import PokemonCard from './components/PokemonCard';
+import { useState } from 'react';
 
 const pokemonList = [
   {
@@ -7,14 +8,39 @@ const pokemonList = [
       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
   },
   {
+    name: 'charmander',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
+  },
+  {
+    name: 'squirtle',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png',
+  },
+  {
+    name: 'pikachu',
+    imgSrc:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+  },
+  {
     name: 'mew',
   },
 ];
-const pokemon = pokemonList[1];
+
+// const pokemon = pokemonList[1];
 function App() {
+  const [pokemonIndex, setPokemon] = useState(0);
+
   return (
     <div>
-      <PokemonCard pokemon={pokemon} />
+      {pokemonIndex > 0 && (
+        <button onClick={() => setPokemon(pokemonIndex - 1)}>Précedent</button>
+      )}
+
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={() => setPokemon(pokemonIndex + 1)}>Suivant</button>
+      )}
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
   );
 }
